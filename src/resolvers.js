8b92b8f08d26db131cd00332da41513954f9079d
Waseem Khan <waseem@paklogics.com>
2022-12-28 05:21:46 -0500

    added the stats totalProductPurchased, totalAmout, avgAmout, totalViews.

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,26 +1,75 @@
 export default {
 	Query: {
-		async totalProductsPurchased(parent, args, context, info) {
+		async marketPlaceStats(parent, args, context, info) {
 			console.log("This is the analytics api.");
-			let { Orders } = context.collections;
-			let totalProductPurchased = await Orders.aggregate( [
-				// { 
-				// 	$project: { 
-						
-				// 		average: {
-				// 			$avg: "$payments.amount"
-				// 		} 
-				// 	},
-				// }
+			let { Orders, Products } = context.collections;
+
+			// total amout purchased from marketplace
+			let totalAmount = await Orders.aggregate( [
+				{ 
+					$project: { 
+						_id: '1',
+						"Amount": {
+							$sum: "$payments.amount"
+						} 
+					},
+				},
 				{
-					$project: {
-						average: {
-							$avg: "$payments.amount"
-						}
-					} 
+					$group: {
+						_id: '1',
+						totalAmount: { $sum : "$Amount" }
+					}
+					 
 				}
 			]).toArray();
-			console.log("totalProductPurchased", totalProductPurchased)
+
+			// total products purchased from marketplace
+			let totalProducts = await Orders.countDocuments({});
+
+			// average of amount purchased from marketplace
+			let avgAmount = await Orders.aggregate([
+				{ 
+					$project: { 
+						_id: '1',
+						"Amount": {
+							$sum: "$payments.amount"
+						} 
+					},
+				},
+				{
+					$group: {
+						_id: '1',
+						avgAmount: { $avg : "$Amount" }
+					}
+					 
+				}
+			]).toArray();
+
+			let totalProductViews = await Products.aggregate([
+				{ 
+					$match : { type : "simple" }
+				},
+				{
+					$project: {
+						_id: '1',
+						"totalViews": { $sum : "$productViews" }
+					}
+				},
+				{
+					$group: {
+						_id: '1',
+						totalViews: { $sum : "$totalViews" }
+					}
+				}
+			]).toArray();
+
+			console.log("totalAmount", totalAmount, "totalProducts", totalProducts, "avgAmount", avgAmount, "totalProductViews", totalProductViews)
+			return {
+				totalProducts,
+				totalAmount: totalAmount?.[0]?.totalAmount,
+				avgAmount: avgAmount?.[0]?.avgAmount,
+				totalViews: totalProductViews?.[0]?.totalViews
+			}
 		}
 	}
 }
